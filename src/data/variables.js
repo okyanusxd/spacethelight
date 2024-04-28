cf9f2ecc6926c7d469690187c06fe73af557518f
okyanusxd <166385116+okyanusxd@users.noreply.github.com>
2024-04-28 14:06:37 +0300

    Update variables.js

--- src/data/variables.js
+++ src/data/variables.js
@@ -2,7 +2,7 @@ export default {
 
     userId: "1174026001072783514",
     github: "spacethelight",
-    description: "My name is Just Nyde. I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
+    description: "My name is Space. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
         description: "Hi, I’m <span class='text-primary'>@spacethelight</span>. I'm currently learning I am currently living in İstanbul, Turkey.",
         liveIn: "İstanbul",
