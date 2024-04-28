832a4c50a07032f4e26ba3a11521e864c1f1f417
okyanusxd <166385116+okyanusxd@users.noreply.github.com>
2024-04-28 14:03:37 +0300

    Update variables.js

--- src/data/variables.js
+++ src/data/variables.js
@@ -1,10 +1,10 @@
 export default {
 
-    userId: "642752306441617417",
-    github: "justnyde",
+    userId: "1174026001072783514",
+    github: "spacethelight",
     description: "My name is Just Nyde. I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
-        description: "Hello. My name is <span class='text-primary'>Just Nyde</span>. I am <span class='text-primary'>16 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
+        description: "Hi, I’m <span class='text-primary'>@spacethelight</span>. I'm currently learning I am currently living in İstanbul, Turkey.",
         liveIn: "İstanbul",
         birthday: "16 October",
         gender: "Man"
